0b3fa28e65946479b5cabfd4aaf860adc9df2732
rivaldoyoseps <rivaldo.yoseps@gmail.com>
2025-06-12 04:55:34 +0700

    refactor: centralize all proto definitions in root 'apis/proto' directory

---
+++ booking-service/internal/client/schedule/client.go
@@ -0,0 +1,11 @@
+package schedule
+
+import (
+	schedulepb "github.com/rivaldoyoseps/schedule-service/api/proto_gen/schedulepb"
+)
+
+func test() {
+	var client schedulepb.ScheduleServiceClient
+
+	_ = client
+}
